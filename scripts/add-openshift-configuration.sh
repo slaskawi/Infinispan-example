b68c73d0f358cfdfe23d1d4b538882b28975bb5d
Sebastian Laskawiec <slaskawi@redhat.com>
2016-05-09 09:42:39 +0200

    Debug logging in the configuration

--- scripts/add-openshift-configuration.sh
+++ scripts/add-openshift-configuration.sh
@@ -10,10 +10,11 @@ CONFIG_PATCH=$CONFIGURATION_HOME/$PATCH_NAME
 
 function make-configuration() {
   echo "==== MAKING CONFIGURATION ===="
-  cp $INFINISPAN_HOME/standalone/configuration/$BASE_CONFIG_FILE $INFINISPAN_HOME/standalone/configuration/$OPENSHIFT_CONFIG_FILE
-  cp $CONFIG_PATCH $INFINISPAN_HOME/standalone/configuration
-  patch -d $INFINISPAN_HOME/standalone/configuration $OPENSHIFT_CONFIG_FILE -i $PATCH_NAME > /dev/null
-  rm $INFINISPAN_HOME/standalone/configuration/$PATCH_NAME
+  #cp $INFINISPAN_HOME/standalone/configuration/$BASE_CONFIG_FILE $INFINISPAN_HOME/standalone/configuration/$OPENSHIFT_CONFIG_FILE
+  #cp $CONFIG_PATCH $INFINISPAN_HOME/standalone/configuration
+  #patch -d $INFINISPAN_HOME/standalone/configuration $OPENSHIFT_CONFIG_FILE -i $PATCH_NAME > /dev/null
+  #rm $INFINISPAN_HOME/standalone/configuration/$PATCH_NAME
+  cp $CONFIGURATION_HOME/$OPENSHIFT_CONFIG_FILE $INFINISPAN_HOME/standalone/configuration
 }
 
 make-configuration
