2bb2bc6f2ed1c208cdd465c4da48f74d45a10e3e
Sebastian Laskawiec <slaskawi@redhat.com>
2016-05-09 08:08:25 +0200

    Permissions for bootstrap files

--- scripts/add-openshift-bootstrap-scripts.sh
+++ scripts/add-openshift-bootstrap-scripts.sh
@@ -4,8 +4,14 @@ SCRIPT_DIR=$(dirname $0)
 INFINISPAN_HOME=/opt/jboss/infinispan-server
 
 function copy-bootstrap() {
-  echo "==== COPYING FILES ===="
+  echo "==== COPYING BOOTSTRAP FILES ===="
   cp -r $SCRIPT_DIR/openshift-bootstrap/* $INFINISPAN_HOME/bin
 }
 
+function update-permissions() {
+  echo "==== UPDATING PERMISSIONS ===="
+  chmod +x $INFINISPAN_HOME/bin/*.sh
+}
+
 copy-bootstrap
+update-permissions
